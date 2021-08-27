9ef663fac3f9c111ac3815ae15fa3487820f5231
lqd1434 <1434288209@qq.com>
2021-08-28 00:02:25 +0800

    feat:开始测试

--- packages/event/src/getUser.ts
+++ packages/event/src/getUser.ts
@@ -1,8 +1,10 @@
 import { setToWindow } from 'u-webView-event/dist/declarations'
 import { getUserInfo } from 'u-webView-core'
 
-
-window.addEventListener('DOMContentLoaded', () => {
+export function setWindow() {
   setToWindow({method:getUserInfo,name:'getUserInfo'})
-  console.log('DOMContentLoaded')
-});
+  window.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded')
+  });
+}
+
