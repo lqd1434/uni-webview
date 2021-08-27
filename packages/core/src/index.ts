015322fcf72832aa8b9096ea7aba9baa35809211
lqd1434 <1434288209@qq.com>
2021-08-27 22:32:39 +0800

    feat:开始测试

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,7 @@
-export * from 'u-webview-event'
-export * from 'u-webview-type'
 export * from './getUserInfo'
-import { U_window } from './gloable'
-import { ResType } from 'u-webview-type'
+export * from './gloable'
 
 
 export function core() {
-  const data:ResType = {status:0,body:'hello I am core',desc:'发送消息'}
-  U_window.JsBridge.postMessage(JSON.stringify(data))
-  alert(JSON.stringify(U_window.JsBridge))
+  console.log(core)
 }
