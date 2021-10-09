ace6d00e5086b78d6bcf8311bd076e265a420565
lqd1434 <1434288209@qq.com>
2021-10-09 16:02:28 +0800

    feat:新增控制台自定义banner(u-webview)

--- packages/hooks/src/useApp.ts
+++ packages/hooks/src/useApp.ts
@@ -9,8 +9,16 @@ export const useApp = ():boolean=>{
   const [isConnect,setConnect] = useState(false)
 
   useLayoutEffect(() => {
+    console.log(paint)
     uWebView.connect((data => setConnect(data)))
   },[])
 
   return isConnect
 }
+
+const paint = `                          _          _               
+  _   _     __      _____| |____   _(_) _____      __
+ | | | |____\\ \\ /\\ / / _ \\ '_ \\ \\ / / |/ _ \\ \\ /\\ / /
+ | |_| |_____\\ V  V /  __/ |_) \\ V /| |  __/\\ V  V / 
+  \\__,_|      \\_/\\_/ \\___|_.__/ \\_/ |_|\\___| \\_/\\_/  
+                                                     `
